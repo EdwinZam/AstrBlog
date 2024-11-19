30735c52fc99e05042f6193a9738e17233e3970a
Edwin Zambrano <eaespinosa9312@hotmail.com>
2024-11-19 11:40:41 -0500

    Blog Completo

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,19 +1,43 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 const blogColletion = defineCollection({
     type: 'content',
-    schema: z.object({
+    schema: ({image}) => z.object({
         title: z.string(),
         date: z.date(),
         description: z.string(),
-        image: z.string(),
+        image: image().refine((img)=> img.width < 2000,{
+            message: 'Image should be greater that 5000px'
+        }),
         //relacion
-        author: z.string(),
+        // author: z.string(),
+
+        author: reference('author'),
+
         //relacion
         tags: z.array(z.string()),
+
+        //Boolean
+        isDraft: z.boolean().default(false),
     })
 });
 
+const authorCollection = defineCollection({
+    type: 'data',
+    schema: ({image}) => z.object({
+        name: z.string(),
+        avatar: image(),
+        twitter: z.string(),
+        linkedIn : z.string(),
+        github : z.string(),
+        bio : z.string(),
+        subtitle: z.string(),
+    })
+
+})
+
 export const collections = {
     blog: blogColletion,
+    author: authorCollection,
 }
+
